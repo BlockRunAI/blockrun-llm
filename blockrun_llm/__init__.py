ab71c778c13dc114b366083f551d40bda3423e1b
1bcMax <viewitter@gmail.com>
2026-03-13 13:16:05 -0400

    chore: bump to v0.8.1

--- blockrun_llm/__init__.py
+++ blockrun_llm/__init__.py
@@ -115,7 +115,7 @@ from .solana_wallet import (
 )
 from .cache import clear_cache, get_cost_log_summary
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 __all__ = [
     "LLMClient",
     "AsyncLLMClient",
